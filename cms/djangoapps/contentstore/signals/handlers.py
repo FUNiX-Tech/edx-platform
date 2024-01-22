f5eef6c42857a4224dbb52c1e3ff0a7779452840
Admin <hoangbnmfx19875@funix.edu.vn>
2024-01-22 23:28:53 +0700

    fix : continue fix delay

--- cms/djangoapps/contentstore/signals/handlers.py
+++ cms/djangoapps/contentstore/signals/handlers.py
@@ -143,7 +143,7 @@ def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=
     # Finally, call into the course search subsystem
     # to kick off an indexing action
     if CoursewareSearchIndexer.indexing_is_enabled() and CourseAboutSearchIndexer.indexing_is_enabled():
-        update_search_index.apply_async((course_key_str, datetime.now(UTC).isoformat()), countdown=30)
+        update_search_index.apply_async((course_key_str, datetime.now(UTC).isoformat()), countdown=0.1)
     update_discussions_settings_from_course_task.apply_async(
         args=[course_key_str],
         countdown=settings.DISCUSSION_SETTINGS['COURSE_PUBLISH_TASK_DELAY'],
